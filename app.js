eb72f8c7679826d0772784c5ce2e24c85e932338
AndrewSotooka <Andrew_XalfaX@yahoo.com.br>
2024-03-28 13:30:08 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let numberOfDrumButtons = document.querySelectorAll('.drum').length
+
+for(let i = 0; i < numberOfDrumButtons; i++) {
+
+    document.querySelectorAll('button')[i].addEventListener('click', function() {
+        let audio = new Audio('./sounds/tom-1.mp3')
+        audio.play()
+
+    })
+}
+
