0785acd5407c951c5f2eeca6b948259b8ae43bc9
AndrewSotooka <Andrew_XalfaX@yahoo.com.br>
2024-03-31 16:15:01 -0300

    ok

--- app.js
+++ app.js
@@ -3,9 +3,46 @@ let numberOfDrumButtons = document.querySelectorAll('.drum').length
 for(let i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll('button')[i].addEventListener('click', function() {
-        let audio = new Audio('./sounds/tom-1.mp3')
-        audio.play()
+
+        let buttonInnerHTML = this.innerHTML
+
+        switch (buttonInnerHTML) {
+            case 'w':
+                let tom1 = new Audio('./sounds/tom-1.mp3');
+tom1.play()
+                break;
+            case 'a':
+                let tom2 = new Audio('./sounds/tom-2.mp3');
+tom2.play()
+                break;
+            case 's':
+                    let tom3 = new Audio('./sounds/tom-3.mp3');
+    tom3.play()
+                break;
+            case 'd':
+                let tom4 = new Audio('./sounds/tom-4.mp3');
+tom4.play()
+                break;
+            case 'j':
+                    let tom5 = new Audio('./sounds/snare.mp3');
+    tom5.play()
+                break;
+            case 'k':
+                    let tom6 = new Audio('./sounds/crash.mp3');
+    tom6.play()
+                break;
+            case 'l':
+                    let tom7 = new Audio('./sounds/kick-bass.mp3');
+    tom7.play()
+                break;
+            default: console.log(buttonInnerHTML);
+                break;
+        }
 
     })
 }
 
+/*
+let audio = new Audio('./sounds/tom-1.mp3')
+audio.play()
+*/
