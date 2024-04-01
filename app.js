4752698ef79f6981722a5f4c17d8d8bd79169f0f
AndrewSotooka <Andrew_XalfaX@yahoo.com.br>
2024-04-01 08:13:01 -0300

    ok

--- app.js
+++ app.js
@@ -6,43 +6,46 @@ for(let i = 0; i < numberOfDrumButtons; i++) {
 
         let buttonInnerHTML = this.innerHTML
 
-        switch (buttonInnerHTML) {
-            case 'w':
-                let tom1 = new Audio('./sounds/tom-1.mp3');
-tom1.play()
-                break;
-            case 'a':
-                let tom2 = new Audio('./sounds/tom-2.mp3');
-tom2.play()
-                break;
-            case 's':
-                    let tom3 = new Audio('./sounds/tom-3.mp3');
-    tom3.play()
-                break;
-            case 'd':
-                let tom4 = new Audio('./sounds/tom-4.mp3');
-tom4.play()
-                break;
-            case 'j':
-                    let tom5 = new Audio('./sounds/snare.mp3');
-    tom5.play()
-                break;
-            case 'k':
-                    let tom6 = new Audio('./sounds/crash.mp3');
-    tom6.play()
-                break;
-            case 'l':
-                    let tom7 = new Audio('./sounds/kick-bass.mp3');
-    tom7.play()
-                break;
-            default: console.log(buttonInnerHTML);
-                break;
-        }
+        makeSounds(buttonInnerHTML)
 
     })
 }
 
-/*
-let audio = new Audio('./sounds/tom-1.mp3')
-audio.play()
-*/
+document.addEventListener('keydown', function(event) {
+    makeSounds(event.key)
+})
+
+function makeSounds(key) {
+    switch (key) {
+        case 'w':
+            let tom1 = new Audio('./sounds/tom-1.mp3');
+tom1.play()
+            break;
+        case 'a':
+            let tom2 = new Audio('./sounds/tom-2.mp3');
+tom2.play()
+            break;
+        case 's':
+                let tom3 = new Audio('./sounds/tom-3.mp3');
+tom3.play()
+            break;
+        case 'd':
+            let tom4 = new Audio('./sounds/tom-4.mp3');
+tom4.play()
+            break;
+        case 'j':
+                let tom5 = new Audio('./sounds/snare.mp3');
+tom5.play()
+            break;
+        case 'k':
+                let tom6 = new Audio('./sounds/crash.mp3');
+tom6.play()
+            break;
+        case 'l':
+                let tom7 = new Audio('./sounds/kick-bass.mp3');
+tom7.play()
+            break;
+        default: console.log(buttonInnerHTML);
+            break;
+    }
+}
